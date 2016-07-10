ef44900172d9a16381017db02a2b797c19331209
ubiquitousT <ubiquitousT@gmail.com>
2016-07-09 20:10:46 -0700

    changes to html/js/css file(s)

--- pages/get-the-best/js/get-the-best.js
+++ pages/get-the-best/js/get-the-best.js
@@ -88,7 +88,7 @@ $(".artist-search").on("keydown",function(event){
 $(document).on("click",".play",function(event){
   // swap glyphicons by giving new values to
   // class property
-  console.log("classes: ", $(".top-tracks span").attr("class"));
+  console.log("top-tracks divs: ", $(".top-tracks div div"));
   $(event.target).attr("class", "glyphicon glyphicon-pause pause").css("display", "inline-block");
 
   var songUrl = $(this).attr("url");
