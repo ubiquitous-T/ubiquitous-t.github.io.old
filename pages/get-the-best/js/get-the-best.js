221cfe1b93a5ebfbfa1dd7f55131e4e85284282b
ubiquitousT <ubiquitousT@gmail.com>
2016-07-09 23:15:43 -0700

    changes to html/js/css file(s)

--- pages/get-the-best/js/get-the-best.js
+++ pages/get-the-best/js/get-the-best.js
@@ -90,7 +90,7 @@ $(document).on("click",".play",function(event){
   // class property
   var $div = $(".top-tracks div div");
 
-  console.log("span class: ", $div[0].$("span"));
+  console.log("span class: ", $($div[0]+" span"));
 
   $(event.target).attr("class", "glyphicon glyphicon-pause pause").css("display", "inline-block");
 
