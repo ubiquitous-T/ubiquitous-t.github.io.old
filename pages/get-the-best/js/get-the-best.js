e84464ec5566251bfe4dbc8f994c358da64a6f3e
ubiquitousT <ubiquitousT@gmail.com>
2016-07-10 17:33:31 -0700

    changes to html/js/css file(s)

--- pages/get-the-best/js/get-the-best.js
+++ pages/get-the-best/js/get-the-best.js
@@ -91,11 +91,11 @@ $(document).on("click",".play",function(event){
   var $target = $(".top-tracks span");
 
   for (var i = 0; i < $target.length; i++) {
-    var classes = $target.attr("class").split(" ");
+    var classes = $target[i].attr("class").split(" ");
     console.log("classes: ", classes);
     if (classes[2] === "pause") {
-      console.log($target.attr("class"));
-      $target.attr("class", classes[0]+" glyphicon-play play").css("display", "inline-block");
+      console.log($target[i].attr("class"));
+      $target[i].attr("class", classes[0]+" glyphicon-play play").css("display", "inline-block");
     }
   }
 
